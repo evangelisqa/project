db01d32fe31fbb8c2d98c6952394d93108d9d88c
evangelisqa <albesaevan@icloud.com>
2024-07-17 00:51:06 -0700

    add

--- scripts/contactus.js
+++ scripts/contactus.js
@@ -5,9 +5,10 @@ document.getElementById('contact-form').addEventListener('submit', function(even
 });
 
 
+// Display the current year in the first paragraph of the footer.
 const currentYear = new Date().getFullYear();
 document.getElementById("currentyear").textContent = currentYear;
 
-// Output the date the document was last modified in the footer's second paragraph
+// Display the date the document was last modified in the second paragraph of the footer.
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
