7639e47d6d678a98714f2f1baa6c109908fe6c48
evangelisqa <albesaevan@icloud.com>
2024-07-21 18:50:34 -0700

    add

--- scripts/stories.js
+++ scripts/stories.js
@@ -5,3 +5,13 @@ document.getElementById("currentyear").textContent = currentYear;
 // Output the date the document was last modified in the footer's second paragraph
 const lastModified = document.lastModified;
 document.getElementById("lastModified").textContent = "Last Modification: " + lastModified;
+
+const hamburger = document.querySelector('.hamburger');
+const mainMenu =document.querySelector('.mainMenu');
+hamburger.addEventListener('click',()=>{
+    mainMenu.classList.toggle('active');
+});
+
+const shareStoryBtn = document.querySelector('share-story-btn');
+shareStoryBtn.addEventListener('click',() =>)
+alert('Thank you for your interest in sharing your story! Please contact us for more detail.')
